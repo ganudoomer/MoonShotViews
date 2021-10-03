f09655ce3a2ec5ed80866f2b0595eee5c47b8d26
ganudoomer <ganuiscool@gmail.com>
2021-10-03 12:38:23 +0530

    Initial Commit

---
+++ MoonShot/MoonShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonShotApp.swift
+//  MoonShot
+//
+//  Created by Sree on 03/10/21.
+//
+
+import SwiftUI
+
+@main
+struct MoonShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
