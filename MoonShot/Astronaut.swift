35b07f66907cf7962b205d73442ea950807e9430
ganudoomer <ganuiscool@gmail.com>
2021-10-04 22:17:12 +0530

    Project 8

---
+++ MoonShot/Astronaut.swift
@@ -0,0 +1,16 @@
+//
+//  Astronaut.swift
+//  MoonShot
+//
+//  Created by Sree on 03/10/21.
+//
+
+import Foundation
+
+struct Astronaut : Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
+
+
---
+++ MoonShot/MissionView.swift
@@ -0,0 +1,8 @@
+//
+//  MissionView.swift
+//  MoonShot
+//
+//  Created by Sree on 03/10/21.
+//
+
+import Foundation
